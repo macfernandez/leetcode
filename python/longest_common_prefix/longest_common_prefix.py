f7f1bf3a784fdfb7ddb4161df812a5e5f085f04d
Macarena Fernandez Urquiza <m.fernandezurquiza@gmail.com>
2022-06-04 14:33:05 -0300

    add solution to #14 problem

---
+++ python/longest_common_prefix/longest_common_prefix.py
@@ -0,0 +1,26 @@
+from typing import *
+
+class Solution:
+    def longest_common_prefix(self, strs: List[str]) -> str:
+        '''
+            Given a list with strings, return the common prefix for those
+            elements.
+
+            Parameters
+            ----------
+            strs: list
+                List with strings.
+
+            Returns
+            -------
+            solution: str
+                Common prefix for the strings in list. If there is no shared
+                prefix, '' is returned.
+        '''
+        solution = ''
+        for i in range(1,len(strs[0])+1):
+            prefix = strs[0][:i]
+            if all([s.startswith(prefix) for s in strs]):
+                solution = strs[0][:i]
+            else:
+                return solution
---
+++ python/longest_common_prefix/test_longest_common_prefix.py
@@ -0,0 +1,10 @@
+import pytest
+
+from python.longest_common_prefix.longest_common_prefix import Solution
+
+@pytest.mark.parametrize('strs, expected_prefix',[
+    (["flower","flow","flight"], 'fl'),
+    (["dog","racecar","car"], '')
+])
+def test_given_list_when_add_longest_common_prefix_runs_then_returns_expected_common_prefix(strs, expected_prefix):
+    assert Solution().longest_common_prefix(strs) == expected_prefix
