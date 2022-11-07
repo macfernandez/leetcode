2b6a8ba259342060bf7faf6b91deced2773ebd88
Macarena Fernandez Urquiza <murquiza@uma-health.com>
2022-11-07 16:50:09 +0100

    add docstrings for problem #58

--- python/length_of_last_word/length_of_last_word.py
+++ python/length_of_last_word/length_of_last_word.py
@@ -1,5 +1,20 @@
 class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
+    def length_of_last_word(self, s: str) -> int:
+        '''
+            Given string s consisting of words and spaces, return the length of
+            the last word in the string.
+
+
+            Parameters
+            ----------
+            s: str
+                String of words and spaces.
+
+            Returns
+            -------
+            last_word_length: int
+                Length of the last word in the string.
+        '''
         last_word = s.strip().split()[-1]
         last_word_length = len(last_word)
         return last_word_length
