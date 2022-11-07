410a156cec4d2730244f88e8677177869b5c325d
Macarena Fernandez Urquiza <murquiza@uma-health.com>
2022-11-07 16:19:33 +0100

    add test for #2 problem

--- python/add_two_numbers/add_two_numbers.py
+++ python/add_two_numbers/add_two_numbers.py
@@ -32,27 +32,3 @@ class Solution:
         if remain:
             solution.append(remain)
         return solution
-
-#print(Solution().addTwoNumbers([2,4,3],[5,6,4]))
-#print(Solution().addTwoNumbers([0],[0]))
-#print(Solution().addTwoNumbers([9,9,9,9,9,9,9],[9,9,9,9]))
-        
-
-# Input: l1 = [2,4,3], l2 = [5,6,4]
-# Output: [7,0,8]
-# Explanation: 342 + 465 = 807.
-
-# Input: l1 = [0], l2 = [0]
-# Output: [0]
-
-# Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
-# Output: [8,9,9,9,0,0,0,1]
-
-9 | 9 | 0 | 18 | 8
-9 | 9 | 1 | 19 | 9
-9 | 9 | 1 | 19 | 9
-9 | 9 | 1 | 19 | 9
-9 | 0 | 1 | 10 | 0
-9 | 0 | 1 | 10 | 0
-9 | 0 | 1 | 10 | 0
-0 | 0 | 1 | 1  | 1
