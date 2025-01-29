1d4a4ca58a251c79751609df507feaa0235c84a1
Macarena Fernandez Urquiza <m.fernandezurquiza@gmail.com>
2025-01-29 12:25:55 -0300

    [top interview 150] #88 order O(n)

--- top_interview_150/merged_sorted_array/merged_sorted_array.py
+++ top_interview_150/merged_sorted_array/merged_sorted_array.py
@@ -9,28 +9,13 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        while m < len(nums1):
-            _ = nums1.pop()
-
-        if not nums1:
-            nums1.extend(nums2[:n])
-            return
-
-        if not nums2[:n]:
-            return
-
-        else:
-            i, p1, p2 = 0, 0, 0
-            while i < m+n:
-                if len(nums1) == m+n:
-                    break
-                n1, n2 = nums1[p1], nums2[p2]
-                if n1 < n2:
-                    if len(nums1)-1 <= p1:
-                        nums1.append(n2)
-                        p2 += 1
-                else:
-                    nums1.insert(i, n2)
-                    p2 += 1
-                p1 += 1
-                i += 1
+        p1, p2, pointer = m-1, n-1, m+n-1
+        
+        while 0 <= p2:
+            if (0 <= p1) and (nums2[p2] < nums1[p1]):
+                nums1[pointer] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[pointer] = nums2[p2]
+                p2 -= 1
+            pointer -= 1
